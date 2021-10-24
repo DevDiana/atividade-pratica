bff2761012a3e9e59047d44ea3f76a40fcbcea03
Diana Regina <dregina@sccalpha.local>
2021-10-24 18:40:15 -0300

    enviando atividade pratica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { arquivo } from './arquivo'; // fiz o importe do arquivo para que eu possa pegar os dados criados nele
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'atividadepratica';
+  lista_alunos = arquivo; // aqui estou passando o ista_alunos para que eu possa pegar os dados do aqruivo e chamar o ngFor passando a variável "alunos"
 }
---
+++ src/app/arquivo.ts
@@ -0,0 +1,36 @@
+export const arquivo = [
+  //nesse arquivo estou passando os dados dos alunos array por array para que possa aparecer no ngFor no html corretamente
+  {
+    //meus dados
+    nome: 'Diana Regina',
+    RU: 1108615,
+    nomeDoCurso: 'Análise e Desenvolvimento de Sistemas',
+    dataAniversario: '26/02/1996',
+  },
+  //separei por "," para que o computador entenda que são dados diferentes
+  //daqui para baixo são dados fictícios
+  {
+    nome: 'Patricia Regina',
+    RU: 1108758,
+    nomeDoCurso: 'Pedagogia',
+    dataAniversario: '11/02/1973',
+  },
+  {
+    nome: 'Roberto Carlos',
+    RU: 1708645,
+    nomeDoCurso: 'Sistemas de Informação',
+    dataAniversario: '20/07/1974',
+  },
+  {
+    nome: 'Samantha Gabriely',
+    RU: 1104516,
+    nomeDoCurso: 'Medicina',
+    dataAniversario: '01/09/1994',
+  },
+  {
+    nome: 'Camile Vitória',
+    RU: 1471561,
+    nomeDoCurso: 'Arquitetura',
+    dataAniversario: '19/04/1997',
+  },
+];
